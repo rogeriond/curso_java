85efd4d9b28f716a248c7202229b02738dd43008
Seu Nome <seu_email_do_github@exemplo.com>
2026-05-12 23:46:17 -0300

    feat: estrutura inicial do curso e exercicio 01

---
+++ src/main/java/secao04_estrutura_sequencial/Exercicio01.java
@@ -0,0 +1,29 @@
+package secao04_estrutura_sequencial;
+
+import java.util.Locale;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.23232;
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+        System.out.printf("%s, which price is $ %.2f%n%n", product2, price2);
+        System.out.printf("Record: %d years old, code %d and gender: %c%n%n", age, code, gender);
+        System.out.printf("Measure: %.8f%n", measure);
+        System.out.printf("Rounded: %.3f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal: %.3f%n", measure);
+    }
+}
